645808c4c11b6a4b5bdf08c4745572c884f801c6
udkumar <udkumar@hotmail.com>
2017-02-23 17:07:13 +0530

    markdown file working

--- word_mark.rb
+++ word_mark.rb
@@ -8,6 +8,11 @@ name_file = "test"
 directory_name = "output_folder"
 Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
+t = ""
+array_desc = []
+heading_hash = {}
+temp = ""
+output = ""
 Dir.glob("**/*.docx") do |file_name|
   doc = Docx::Document.open(file_name)
 
@@ -22,27 +27,48 @@ Dir.glob("**/*.docx") do |file_name|
       end
     end
   end
-end
 
-file_data.each_with_index do |l, d|
-  if l.include? file_data[d]
+  file_data.each_with_index do |l, d|
+    if l.include? file_data[d]
 
-    if (l[0].to_i != 0)
-      md_file_name = file_data[d].split(".")
-      md_file_name.each do |r|
-        output_name = "#{directory_name}/#{File.basename(r, '.*')}.md"
-        output = File.open(output_name, 'w')
-        
-        md_file_heading = file_data[d+1]
-        md_file_description = file_data[d+2]
-        output << "#"+"#{md_file_heading}\n\n"
-        output << "#{md_file_description} \n"
+      if ((l.strip)[0].to_i != 0)
+        md_file_name = file_data[d].split(".")
+
+        t = file_data[d+1]
+
+        if(array_desc.size > 0)
+          heading_hash[temp] = array_desc
+          array_desc = []
+        end 
+      else
+        if(t != l)
+          array_desc << l
+          temp = t
+        end
       end
-      
     end
   end
-end
 
+  if(array_desc.size> 0)
+    heading_hash[temp] = array_desc
+    array_desc = []
+  end
+
+  heading_hash.each_with_index do |(k, v), i|
+    fine_name = (i).to_s
+    if k != ""
+      output_name = "#{directory_name}/#{File.basename(fine_name, '.*')}.md"
+      output = File.open(output_name, 'w')
+
+      output << "#"+"#{k}\n\n"
+      v.each do |des|
+        output << "#{des} \n"
+      end
+    end    
+  end
+
+end
+  
 output.close
 
 
