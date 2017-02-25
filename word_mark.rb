cbd8da54c2de8fb37ec21af3b3384992335944ed
udkumar <udkumar@hotmail.com>
2017-02-26 02:41:08 +0530

    make text in folder

--- word_mark.rb
+++ word_mark.rb
@@ -5,14 +5,19 @@ require 'docx'
 file_data = []
 name_file = "test"
 
-directory_name = "output_folder"
-Dir.mkdir(directory_name) unless File.exists?(directory_name)
+# directory_name = "output_folder"
+# Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
 t = ""
 array_desc = []
 heading_hash = {}
 temp = ""
 output = ""
+folder_name = ""
+directory_name = ""
+
+flag = true
+count = 0
 Dir.glob("**/*.docx") do |file_name|
   doc = Docx::Document.open(file_name)
 
@@ -22,7 +27,7 @@ Dir.glob("**/*.docx") do |file_name|
     table.rows.each do |row| # Row-based iteration
       row.cells.each_with_index do |cell, i|
         if i == 2 
-          file_data << cell.text
+          file_data << cell.text.gsub('=','')
         end
       end
     end
@@ -30,10 +35,17 @@ Dir.glob("**/*.docx") do |file_name|
 
   file_data.each_with_index do |l, d|
     if l.include? file_data[d]
-
+      
       if ((l.strip)[0].to_i != 0)
         md_file_name = file_data[d].split(".")
-
+        
+        #start folder name
+        if flag
+          directory_name =  md_file_name[0].to_i
+          flag =  false
+        end
+        count +=1
+        
         t = file_data[d+1]
 
         if(array_desc.size > 0)
@@ -54,10 +66,15 @@ Dir.glob("**/*.docx") do |file_name|
     array_desc = []
   end
 
-  heading_hash.each_with_index do |(k, v), i|
-    fine_name = (i).to_s
+  puts count-1
+  puts directory_name
+
+  heading_hash.each_with_index do |(k, v), indx|
     if k != ""
-      output_name = "#{directory_name}/#{File.basename(fine_name, '.*')}.md"
+      puts indx
+      directory_name += (indx-1)
+      Dir.mkdir("#{directory_name}") unless File.exists?("#{directory_name}")
+      output_name = "#{directory_name}/#{File.basename(1.to_s, '.*')}.md"
       output = File.open(output_name, 'w')
 
       output << "#"+"#{k}\n\n"
@@ -69,7 +86,7 @@ Dir.glob("**/*.docx") do |file_name|
 
 end
   
-output.close
+
 
 
 
